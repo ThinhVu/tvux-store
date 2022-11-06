f55bb991aa00e220f5c3b19da39b62206d55c5c7
ThinhVu <vutrongthinhk7@gmail.com>
2022-11-06 10:14:05 +0700

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,18 @@
-import { reactive, App } from 'vue'
-import SomeComponent from './SomeComponent.vue'
+// @ts-nocheck
+import { App } from 'vue'
 import { setupDevtools } from './devtools'
-import { MyPluginData } from './data'
 
 // Our plugin
+const data = {}
 
 export default {
   install (app: App, options = {}) {
-    const data = reactive<MyPluginData>({
-      message: 'hello',
-      counter: 0
-    })
-
     let devtools: ReturnType<typeof setupDevtools>
-
-    app.mixin({
-      computed: {
-        $hello () {
-          return data.message
-        }
-      },
-
-      methods: {
-        $doSomething () {
-          const trackEnd = devtools ? devtools.trackStart('$doSomething') : null
-          return new Promise(resolve => {
-            setTimeout(() => {
-              console.log('done')
-              if (trackEnd) trackEnd()
-              resolve('done')
-            }, 1000)
-          })
-        }
-      }
-    })
-
-    app.component('SomeComponent', SomeComponent)
-
-    setInterval(() => {
-      data.counter += 5
-    }, 5000)
-
     if (process.env.NODE_ENV === 'development' || __VUE_PROD_DEVTOOLS__) {
       devtools = setupDevtools(app, data)
     }
+  },
+  create(name: string, store = {}) {
+    data[name] = store
   }
 }
